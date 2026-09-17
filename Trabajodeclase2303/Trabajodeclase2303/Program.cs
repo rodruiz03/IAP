b2c36743230bca4e5fe01948f3b6d54bc7a987b0
rodruiz03 <123915751+rodruiz03@users.noreply.github.com>
2026-09-17 03:20:37 -0600

    Add files via upload

---
+++ Trabajodeclase2303/Trabajodeclase2303/Program.cs
@@ -0,0 +1,37 @@
+﻿//Rodrigo Ruiz- 1037623
+string DivisionesEnBilletes(int numero)
+{
+    int[] denominaciones = { 100, 50, 20, 10, 5, 1 };
+    int[] cantidades = new int[denominaciones.Length];
+    string resultado = "";
+    Console.ReadLine();
+
+    for (int i = 0; i < denominaciones.Length; i++)
+    {
+        cantidades[i] = numero / denominaciones[i];
+        numero %= denominaciones[i];
+        resultado += $"Billetes de {denominaciones[i]} = {cantidades[i]}\n";
+        Console.ReadLine();
+    }
+    Console.ReadLine();
+    return resultado;
+}
+
+string DivisionesEnMonedas(int numero)
+{
+    int[] denominaciones = { 50, 25, 10, 5, 1 };
+    int[] cantidades = new int[denominaciones.Length];
+    string resultado = "";
+    Console.ReadLine();
+
+    for (int i = 0; i < denominaciones.Length; i++)
+    {
+        cantidades[i] = numero / denominaciones[i];
+        numero %= denominaciones[i];
+        resultado += $"Monedas de {denominaciones[i]} = {cantidades[i]}\n";
+        Console.ReadLine();
+    }
+    Console.ReadLine();
+    return resultado;
+}
+
