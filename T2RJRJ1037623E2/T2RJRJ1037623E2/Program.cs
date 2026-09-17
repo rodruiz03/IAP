9de6a8bc61c69e6a762c523290b526c6d3d781f2
rodruiz03 <123915751+rodruiz03@users.noreply.github.com>
2026-09-17 03:15:52 -0600

    Add files via upload

---
+++ T2RJRJ1037623E2/T2RJRJ1037623E2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Andres De Leon - 1221023");
+        
+        Console.Write("Ingrese una cantidad en Quetzales (0 a 999.99): ");
+        double monto = double.Parse(Console.ReadLine());
+
+        int bil100 = (int)monto / 100;
+        monto %= 100;
+
+        int bill50 = (int)monto / 50;
+        monto %= 50;
+
+        int bill20 = (int)monto / 20;
+        monto %= 20;
+
+        int bill10 = (int)monto / 10;
+        monto %= 10;
+
+        int bill5 = (int)monto / 5;
+        monto %= 5;
+
+        int mon1 = (int)monto;
+        monto %= 1;
+
+        int mon25 = (int)(monto / 0.25);
+        monto %= 0.25;
+
+        int mon1c = (int)(monto / 0.01);
+        monto %= 0.01;
+
+        Console.WriteLine("Billetes de 100 Quetzales: " + bil100);
+
+        Console.WriteLine("Billetes de 50 Quetzales: " + bill50);
+
+        Console.WriteLine("Billetes de 20 Quetzales: " + bill20);
+
+        Console.WriteLine("Billetes de 10 Quetzales: " + bill10);
+
+        Console.WriteLine("Billetes de 5 Quetzales: " + bill5);
+
+        Console.WriteLine("Monedas de 1 Quetzales: " + mon1);
+
+        Console.WriteLine("Monedas de 25 centavos: " + mon25);
+
+        Console.WriteLine("Monedas de 1 centavo: " + mon1c);
+
+    }
+}
+
---
+++ T3RJRJ1037623/T3RJRJ1037623/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Tarea#3 Rodrigo Ruiz-1037623");
+        Console.WriteLine("");
+
+        Console.Write("Ingrese el día de su nacimiento (1-31): ");
+        int dia = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el mes de su nacimiento (1-12): ");
+        int mes = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el año de su nacimiento (yyyy): ");
+        int anio = int.Parse(Console.ReadLine());
+
+        string signo = "";
+        if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18))
+        {
+            signo = "Acuario";
+        }
+        else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20))
+        {
+            signo = "Piscis";
+        }
+        else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19))
+        {
+            signo = "Aries";
+        }
+        else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20))
+        {
+            signo = "Tauro";
+        }
+        else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20))
+        {
+            signo = "Géminis";
+        }
+        else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22))
+        {
+            signo = "Cáncer";
+        }
+        else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22))
+        {
+            signo = "Leo";
+        }
+        else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22))
+        {
+            signo = "Virgo";
+        }
+        else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22))
+        {
+            signo = "Libra";
+        }
+        else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21))
+        {
+            signo = "Escorpio";
+        }
+        else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21))
+        {
+            signo = "Sagitario";
+        }
+        else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19))
+        {
+            signo = "Capricornio";
+        }
+
+        Console.WriteLine("Tu signo del zodiaco es: " + signo);
+    }
+}
+
+
